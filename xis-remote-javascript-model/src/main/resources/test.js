3a36ee7bf987cf53fc84d62357ef54ff260c8834
bernd9 <github@peltzer-bartels.de>
2022-02-04 19:50:27 +0100

    improved html example

--- xis-remote-javascript-model/src/main/resources/test.js
+++ xis-remote-javascript-model/src/main/resources/test.js
@@ -1,8 +1,261 @@
-function findDescendents(element, matcher) {
-      var nodeList = element.childNodes;
+var categories = [
+    {title: 'Küche', products:[
+        {title: 'Kochtopf', price: 12.5},
+        {title: 'Braftpfanne', price: 12.5},
+        {title: 'Topflappen', price: 12.5},
+        {title: 'Espressokocher', price: 12.5}
+    ]},
+    {title: 'Bad', products:[
+        {title: 'Vorleger', price: 12.5},
+        {title: 'Klobürste', price: 12.5}
+    ]}
+
+];
+
+
+function refreshAll(o, elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].refresh(o);
+    }
+}
+
+var widget = {
+    values : {'categories': categories},
+    getValue: function(name) {
+        return this.values[name];
+    },
+
+    refresh: function(container) {
+        debugger;
+        clearChildNodes(container);
+        this.element = container;
+        refreshAll(this, [categoriesH3, categoriesFor]);
+    },
+    getElement() {
+        return this.element;
+    }
+
 
 }
 
 
+var categoriesH3 = {
+    refresh: function(parent) {
+        this.parent = parent;
+        this.element = appendElement(parent.getElement(), 'h3');
+        refreshAll(this, [categoriesText]);
+    },
+    getValue: function(name) {
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.element;
+    }
+}
 
 
+var categoriesText = {
+    refresh: function(parent) {
+        this.parent = parent;
+        parent.getElement().innerText = 'Kategorien';
+    },
+    getValue: function(name) {
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.parent.getElement();
+    }
+}
+
+var categoriesFor = {
+    names: ['i', 'categoryNumber','category'],
+    refresh: function(parent) {
+        this.parent = parent;
+        this.variables = [];
+        var array = parent.getValue('categories');
+        for (var index = 0; index < array.length; index++) {
+            this.variables['i'] = index;
+            this.variables['categoryNumber'] = index + 1;
+            this.variables['category'] = array[index];  
+            refreshAll(this, [categoryDiv]);
+        }
+    },
+    getValue: function(name) {
+        if (this.names.indexOf(name) != -1) { // may be, we want to return undefined !!!
+            return this.variables[name];
+        }
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.parent.getElement();
+    }
+
+}
+
+
+var categoryDiv =  {
+    refresh: function(parent) {
+        this.parent = parent;
+        this.element = appendElement(parent.getElement(), 'div');
+        refreshAll(this, [categoryH4, productsFor]);
+    },
+    getValue: function(name) {
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.element;
+    }
+}
+
+
+var categoryH4 = {
+    refresh: function(parent) {
+        this.parent = parent;
+        this.element = appendElement(parent.getElement(), 'h4');
+        refreshAll(this, [categoryTitle]);
+    },
+    getValue: function(name) {
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.element;
+    }
+}
+
+
+var categoryTitle = {
+    refresh: function(parent) {
+        //${categoryNumber}. ${category.title}
+        var e = parent.getElement();
+        appendText(e, parent.getValue('categoryNumber'));
+        appendText(e, '. ');
+        appendText(e, parent.getValue('category').title); // ${category.title}
+    },
+    getValue: function(name) {
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.parent.getElement();
+    }
+}
+
+var productsFor = {
+    names: ['i', 'productNumber','product'],
+    refresh: function(parent) {
+        this.parent = parent;
+        this.variables = [];
+        var array = parent.getValue('category').products;
+        for (var index = 0; index < array.length; index++) {
+            this.variables['i'] = index;
+            this.variables['productNumber'] = index + 1;
+            this.variables['product'] = array[index];
+            refreshAll(this, [productDiv]);
+        }
+    },
+    getValue: function(name) {
+        if (this.names.indexOf(name) != -1) { // may be, we want to return undefined !!!
+            return this.variables[name];
+        }
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.parent.getElement();
+    }
+
+}
+
+var productDiv =  {
+    refresh: function(parent) {
+        this.parent = parent;
+        this.element = appendElement(parent.getElement(), 'div');   
+        refreshAll(this, [productDetails]);
+    },
+    getValue: function(name) {
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.element;
+    }
+}
+
+
+// text-content
+var productDetails = {
+    refresh: function(parent) {
+        var e = parent.getElement();
+        var text = '';
+        text += parent.getValue('product').title;
+        text += ' ';
+        text += parent.getValue('product').price;
+        text += ' EUR';
+        e.innerText = text;
+    },
+    getValue: function(name) {
+        return this.parent.getValue(name);
+    },
+    getElement() {
+        return this.parent.getElement();
+    }
+}
+
+
+// Util Functions
+
+function byId(id) {
+    return document.getElementById(id);
+}
+
+
+function hideElement(element) {
+    console.log('hide');
+    element.style = 'display: none';
+}
+
+function showElement(element) {
+    element.style.display = 'display: block';
+}
+
+function clearChildNodes(element) {
+    while(element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
+}
+
+function appendElement(parent, tagName, attributes=[]) {
+    var child = createElement(tagName, attributes);
+    parent.appendChild(child);
+    return child;
+}
+
+function appendText(parent, content) {
+    if (!parent.innerText) {
+        parent.innerText = content;
+    } else {
+        parent.innerText += content;
+    }
+}
+
+function createElement(tagName, attributes=[]) {
+    var e = document.createElement(tagName);
+    for (var name in attributes) {
+       e.setAttribute(name, attributes[name]);
+    }
+    return e;
+}
+
+function nodeListToArray(nodeList) {
+    var rv = [];
+    for (var i = 0; i < nodeList.length; i++) {
+        rv.push(nodeList.item(i));
+    }
+    return rv;
+}
+
+function notEmpty(value) {
+    return value && value.length > 0;
+}
+
+
+function buttonClicked() {
+    widget.refresh(byId('content'));
+}
