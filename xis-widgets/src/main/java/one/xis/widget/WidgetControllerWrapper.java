810196abd470ae8bea84cfc10efec7bff0d9a106
bernd9 <github@peltzer-bartels.de>
2022-10-29 01:04:27 +0200

    fixed overriden with lower accesss

--- xis-widgets/src/main/java/one/xis/widget/WidgetControllerWrapper.java
+++ xis-widgets/src/main/java/one/xis/widget/WidgetControllerWrapper.java
@@ -1,6 +1,5 @@
 package one.xis.widget;
 
-import one.xis.common.RequestContext;
 import one.xis.controller.ControllerModel;
 import one.xis.controller.ControllerWrapper;
 
@@ -8,9 +7,5 @@ class WidgetControllerWrapper extends ControllerWrapper {
 
     WidgetControllerWrapper(Object contoller, ControllerModel controllerModel, Class<?> modelType) {
         super(contoller, controllerModel, modelType);
-    }
-
-    Object invokeInit(RequestContext context) {
-        return null;
     }
 }
