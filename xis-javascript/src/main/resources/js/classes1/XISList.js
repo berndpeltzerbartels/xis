ce3c3f010cd9871e85be334e958c10d37e1261e2
bernd <bernd@peltzer-bartels.de>
2022-11-21 23:40:30 +0100

    added list-js-class

---
+++ xis-javascript/src/main/resources/js/classes1/XISList.js
@@ -0,0 +1,45 @@
+class XISList {
+
+    /**
+     * 
+     * @param {Array} arr 
+     */
+    constructor(arr) {
+        this.className = 'XISList';
+        if (arr) {
+            this.arr = arr;
+        } else {
+            this.arr = [];
+        }
+    }
+
+    add(obj) {
+        this.arr.push(obj);
+    }
+
+    addAll(arr) {
+        for (var e of arr) {
+            this.arr.push(e);
+        }
+    }
+
+    contains(obj) {
+        this.arr.indexOf(obj) != -1;      
+    }
+
+    remove(obj) {
+        var index = this.arr.indexOf(obj);
+        if (index !== -1) {
+            return this.arr.splice(index, 1);
+        }
+    }
+
+    indexOf(obj) {
+        return this.arr.indexOf(obj);
+    }
+
+    isEmpty() {
+        return this.arr.length == 0;
+    }
+
+}
