bf158d7886ce2a3efabccb9d8853e7911f508ec9
bernd9 <github@peltzer-bartels.de>
2023-01-25 23:48:36 +0100

    fixed some tests after seperating parsers and entites

--- xis-template-javascript/src/test/java/one/xis/test/js/api/FunctionTokenParserTest.java
+++ xis-template-javascript/src/test/java/one/xis/test/js/api/FunctionTokenParserTest.java
@@ -5,11 +5,10 @@ import one.xis.test.mocks.Console;
 import org.junit.jupiter.api.Test;
 
 import javax.script.ScriptException;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class FunctionTokenTest {
+class FunctionTokenParserTest {
 
 
     @Test
@@ -19,18 +18,12 @@ class FunctionTokenTest {
         var script = JavaScript.builder()
                 .withApi()
                 .withScript("var result = {};")
-                .withScript("var token = new FunctionToken('abc()');")
-                .withScript("result['success'] = token.tryParse();")
-                .withScript("result['token'] = token;")
-                .withScript("result")
+                .withScript("var parser = new FunctionTokenParser('abc()');")
+                .withScript("parser.tryParse();")
                 .withBinding("console", console)
                 .build();
         var result = script.runForMap();
         var lines = console.getInfoLog();
-        assertThat(result).containsEntry("success", true);
-        var token = (Map<String, Object>) result.get("token");
-        assertThat(token).containsEntry("src", "abc()");
-        assertThat(token).containsEntry("end", 4);
-        assertThat(token).containsEntry("functionName", "abc");
+        assertThat(result).containsEntry("functionName", "abc");
     }
 }
--- xis-template-javascript/src/test/java/one/xis/test/js/api/ScriptExpressionParserTest.java
+++ xis-template-javascript/src/test/java/one/xis/test/js/api/ScriptExpressionParserTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import javax.script.ScriptException;
 
-class ScriptExpressionTest {
+class ScriptExpressionParserTest {
 
     @Test
     void simpleVariable() throws ScriptException {
         JavaScript.builder()
                 .withApi()
                 .withScript("var data = new Data({test: 123});")
-                .withScript("new ScriptExpression('${test}').evaluate(data);")
+                .withScript("new ScriptExpressionParser('${test}').evaluate(data);")
                 .build().assertResultEquals(123);
     }
 
@@ -21,7 +21,7 @@ class ScriptExpressionTest {
         JavaScript.builder()
                 .withApi()
                 .withScript("var data = new Data({appointment: {location: 'Office'}});")
-                .withScript("new ScriptExpression('${appointment.location}').evaluate(data);")
+                .withScript("new ScriptExpressionParser('${appointment.location}').tryParse().evaluate(data);")
                 .build().assertResultEquals("Office");
     }
 
@@ -30,7 +30,7 @@ class ScriptExpressionTest {
         JavaScript.builder()
                 .withApi()
                 .withScript("var data = new Data({saint: 'Nikolaus'});")
-                .withScript("new ScriptExpression('Das ist das Haus vom ${saint}').evaluate(data);")
+                .withScript("new ScriptExpressionParser('Das ist das Haus vom ${saint}').tryParse().evaluate(data);")
                 .build().assertResultEquals("Das ist das Haus vom Nikolaus");
     }
 
@@ -38,7 +38,7 @@ class ScriptExpressionTest {
     void variableWithFunction() throws ScriptException {
         JavaScript.builder()
                 .withApi()
-                .withScript("new ScriptExpression('${formatDate(appointment.state)').script;")
+                .withScript("new ScriptExpressionParser('${formatDate(appointment.state)').tryParse().script;")
                 .build().assertResultEquals("formatDate(data.getValue());");
     }
 
