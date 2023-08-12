46c4ef6d01d875f23ecddc035984a69dd29b226d
bernd9 <github@peltzer-bartels.de>
2023-08-12 22:27:21 +0200

    removed debug-code, deactivated micronaut app

--- xis-integration-tests/src/test/java/test/widget/ActionLinkWithTargetWidgetTest.java
+++ xis-integration-tests/src/test/java/test/widget/ActionLinkWithTargetWidgetTest.java
@@ -31,22 +31,16 @@ class ActionLinkWithTargetWidgetTest {
         assertThat(container1).isNotNull();
         assertThat(container2).isNotNull();
 
-        System.out.println("************** 1 *************");
-        System.out.println(result.getDocument().asString());
         // first widget
         var actionLink1 = container1.getDescendantById("action1");
         assertThat(actionLink1).isNotNull();
         actionLink1.onclick.accept(null);
 
-        System.out.println("************** 2 *************");
-        System.out.println(result.getDocument().asString());
         // second widget is displayed in second container
         var actionLink2 = container2.getDescendantById("action2");
         assertThat(actionLink2).isNotNull();
         actionLink2.onclick.accept(null);
 
-        System.out.println("************** 3 *************");
-        System.out.println(result.getDocument().asString());
         // third widget is displayed in first container
         assertThat(container1.getDescendantById("widget3")).isNotNull();
     }
