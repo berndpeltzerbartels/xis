e4f10c2f94c9383ab2bc3c18d08ae59d073b6642
bernd9 <github@peltzer-bartels.de>
2023-08-30 23:09:22 +0200

    fixed all tests

--- xis-integration-tests/src/test/java/test/page/ActionLinkPageTest.java
+++ xis-integration-tests/src/test/java/test/page/ActionLinkPageTest.java
@@ -31,12 +31,13 @@ class ActionLinkPageTest {
         result.getDocument().getElementById("action-link1").onclick.accept(null);
 
         assertThat(result.getDocument().getElementByTagName("title").innerText).isEqualTo("ActionPage");
-        verify(service, times(2)).getData(); // 2 times, because after action, data has to be reloaded
+        verify(service).getData();
 
         var captor = ArgumentCaptor.forClass(ActionLinkPageData.class);
         verify(service, times(1)).update(captor.capture());
         assertThat(captor.getValue().getId()).isEqualTo(101);
         assertThat(captor.getValue().getValue()).isEqualTo("bla");
+        assertThat(result.getDocument().getElementByTagName("title").innerText).isEqualTo("ActionPage");
 
     }
 
@@ -48,7 +49,7 @@ class ActionLinkPageTest {
         result.getDocument().getElementById("action-link2").onclick.accept(null);
 
         assertThat(result.getDocument().getElementByTagName("title").innerText).isEqualTo("ActionPage");
-        verify(service, times(2)).getData(); // 2 times, because after action, data has to be reloaded
+        verify(service).getData();
         var captor = ArgumentCaptor.forClass(ActionLinkPageData.class);
         verify(service, times(1)).update(captor.capture());
         assertThat(captor.getValue().getId()).isEqualTo(101);
@@ -59,15 +60,15 @@ class ActionLinkPageTest {
     @Test
     void action3() {
         var result = testContext.openPage("/actionPage.html");
-        result.getDocument().getElementById("action-link3").onclick.accept(null); // "action-link3"is set by model variable "action3"
+        result.getDocument().getElementById("action-link3").onclick.accept(null); // "action-link3" is set by model variable "action3"
 
 
-        verify(service, times(1)).getData(); // once, because a new page was loaded after action
+        verify(service).getData(); // once, because a new page was loaded after action
         // redirct to index
         assertThat(result.getDocument().getElementByTagName("title").innerText).isEqualTo("Index");
         assertThat(testContext.getSingleton(IndexPage.class).getInvocations()).isEqualTo(1); // model from next page has to be loaded
         var captor = ArgumentCaptor.forClass(ActionLinkPageData.class);
-        verify(service, times(1)).update(captor.capture());
+        verify(service).update(captor.capture());
         assertThat(captor.getValue().getId()).isEqualTo(101);
         assertThat(captor.getValue().getValue()).isEqualTo("bla");
 
