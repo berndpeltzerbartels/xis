7bca31c7b4092323873b9a90a923c6d7441f195b
bernd9 <bernd@peltzer-bartels.de>
2025-03-25 15:38:06 +0100

    httpconnector -> connector reverted

--- xis-javascript/src/main/js/test/classes/HttpConnectorMock.js
+++ xis-javascript/src/main/js/test/classes/HttpConnectorMock.js
@@ -1,4 +1,4 @@
-class ConnectorMock {
+class HttpConnectorMock {
 
     /**
      * @public
--- xis-javascript/src/main/js/test/classes/TestApplication.js
+++ xis-javascript/src/main/js/test/classes/TestApplication.js
@@ -1,7 +1,7 @@
 class TestApplication {
 
     constructor() {
-        this.httpConnector = new ConnectorMock();
+        this.httpConnector = new HttpConnectorMock();
         this.domAccessor = new DomAccessor();
         this.client = new Client(this.httpConnector);
         this.pages = new Pages(this.client);
