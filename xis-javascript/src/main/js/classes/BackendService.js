c73f876b78eabc6fa0ceb51a9d09bd16b8366cb1
bernd9 <bernd@peltzer-bartels.de>
2025-03-26 16:10:55 +0100

    backendservice

---
+++ xis-javascript/src/main/js/classes/BackendService.js
@@ -0,0 +1,19 @@
+class BackenService {
+
+    triggerPageReloadOnDemand(serverResponse) {
+        if (serverResponse.reloadPage) {
+            app.pageController.triggerPageReload();
+         }
+         return serverResponse;
+    }
+
+    triggerWidgetReloadsOnDemand(serverResponse) {
+        for (var widgetId in serverResponse.reloadWidgets) {
+            var containerHandler = app.widgetContainers.findContainerHandlerByWidgetId(widgetId);
+            if (containerHandler) {
+                containerHandler.triggerWidgetReload();
+            }
+         }
+         return serverResponse;
+    }
+}
