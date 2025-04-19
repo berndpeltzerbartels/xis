cd2b4190d1ee1a97864c6c3673f31c91e11f2d30
bernd9 <bernd@peltzer-bartels.de>
2025-04-19 13:24:56 +0200

    xis-frontend ->xis-adapter-spi

--- xis-remote-core/src/main/java/one/xis/LocalStorage.java
+++ xis-remote-core/src/main/java/one/xis/LocalStorage.java
@@ -8,11 +8,14 @@ import java.lang.annotation.Target;
 /**
  * Annotation allows reading and writing to a local storage.
  * <p>
+ * The annotation placed at a method will write the value of the parameter to the local storage.
+ * This will typically be an action method, but it can also be used by model specific methods.
+ * <p>
  * The value is the key to the local storage. The annotation placed at a method parameter
  * will read the value from the local storage and place it into the parameter.
  * <p>
- * The annotation placed at a method will write the value of the parameter to the local storage.
- * This will typically be an action method, but it can also be used by model specific methods.
+ * The behaviour is similar to the {@link PageScope} annotation, but the local storage
+ * is persistent and will be available even when the page is reloaded or in a new session.
  */
 @Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
