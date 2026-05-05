07360e88b6d1e0243aa2e8d27320cd222e5e5a9f
bernd9 <bernd@peltzer-bartels.de>
2026-05-05 15:22:43 +0200

    micronaut entfernt

--- xis-micronaut-test-app/src/main/groovy/micronaut/example/RepeatInsideRepeatItem.groovy
+++
@@ -1,9 +0,0 @@
-package micronaut.example
-
-class RepeatInsideRepeatItem {
-    def title
-
-    RepeatInsideRepeatItem(title) {
-        this.title = title
-    }
-}
--- xis-micronaut-test-app/src/main/groovy/micronaut/example/RepeatInsideRepeatPageSubItem.groovy
+++
@@ -1,9 +0,0 @@
-package micronaut.example
-
-class RepeatInsideRepeatPageSubItem {
-    def title
-
-    RepeatInsideRepeatPageSubItem(title) {
-        this.title = title
-    }
-}
